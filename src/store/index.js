9bceff4dd64e5bea170d1e5bf499a99255c3d0b7
Uma Victor <umavictor11@gmail.com>
2021-08-28 19:54:21 +0100

    commit setinvoice mutation

--- src/store/index.js
+++ src/store/index.js
@@ -19,7 +19,32 @@ export default createStore({
       const results = await db.collection('invoices').get();
       results.forEach(doc => {
         if(!state.invoiceData.some(invoice > invoice.docId ===doc.id)) {
-          
+          const data = {
+            docId: doc.id,
+            invoiceId: doc.data().invoiceId,
+            billerStreetAddress: doc.data().billerStreetAddress,
+            billerCity: doc.data().billerCity,
+            billerZipCode: doc.data().billerZipCode,
+            billerCountry: doc.data().billerCountry,
+            clientName: doc.data().clientName,
+            clientEmail: doc.data().clientEmail,
+            clientStreetAddress: doc.data().clientStreetAddress,
+            clientCity: doc.data().clientCity,
+            clientZipCode: doc.data().clientZipCode,
+            clientCountry: doc.data().clientCountry,
+            invoiceDateUnix: doc.data().invoiceDateUnix,
+            invoiceDate: doc.data().invoiceDate,
+            paymentTerms: doc.data().paymentTerms,
+            paymentDueDateUnix: doc.data().paymentDueDateUnix,
+            paymentDueDate: doc.data().paymentDueDate,
+            productDescription: doc.data().productDescription,
+            invoiceItemList: doc.data().invoiceItemList,
+            invoiceTotal: doc.data().invoiceTotal,
+            invoicePending: doc.data().invoicePending,
+            invoiceDraft: doc.data().invoiceDraft,
+            invoicePaid: doc.data().invoicePaid,
+          };
+          commit("SET_INVOICE_DATA", data);
         }
       });
     }
